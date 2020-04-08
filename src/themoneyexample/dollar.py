838f24b0f9f5583eebcf564e0d9839b5a48d5f1c
RyuichiYoshimoto <mamimi.mute@gmail.com>
2020-04-09 00:24:22 +0900

    Refactor. add type hint.

--- src/themoneyexample/dollar.py
+++ src/themoneyexample/dollar.py
@@ -2,8 +2,8 @@ class Dollar:
     def __init__(self, amount: int):
         self.amount: int = amount
 
-    def times(self, multiplier: int):
+    def times(self, multiplier: int) -> 'Dollar':
         return Dollar(self.amount * multiplier)
 
-    def equals(self, dollar: 'Dollar'):
+    def equals(self, dollar: 'Dollar') -> bool:
         return self.amount == dollar.amount
--- src/themoneyexample/tests/test_money.py
+++ src/themoneyexample/tests/test_money.py
@@ -8,6 +8,7 @@ class TestMoney:
         five = Dollar(5)
         product: Dollar = five.times(2)
         assert 10 == product.amount
+
         product: Dollar = five.times(3)
         assert 15 == product.amount
 
