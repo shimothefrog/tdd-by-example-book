1e25008a892fb411555730491040b2d8370a44f7
RyuichiYoshimoto <mamimi.mute@gmail.com>
2020-04-09 22:37:57 +0900

    Part1 chapeter4. I couldn't use name-magling style. Becaues it's ofen annoying When it comes to extend class.

--- src/themoneyexample/dollar.py
+++ src/themoneyexample/dollar.py
@@ -1,24 +1,24 @@
 class Dollar:
     def __init__(self, amount: int):
-        self.amount: int = amount
+        self._amount: int = amount
 
     def times(self, multiplier: int) -> 'Dollar':
-        return Dollar(self.amount * multiplier)
+        return Dollar(self._amount * multiplier)
 
     def __eq__(self, other):
         if isinstance(other, Dollar):
-            return self.amount == other.amount
+            return self._amount == other._amount
 
         if isinstance(other, int):
-            return self.amount == other
+            return self._amount == other
 
         raise TypeError()
 
     def __ne__(self, other):
         if isinstance(other, Dollar):
-            return self.amount != other.amount
+            return self._amount != other._amount
 
         if isinstance(other, int):
-            return self.amount != other
+            return self._amount != other
 
         raise TypeError()
--- src/themoneyexample/tests/test_money.py
+++ src/themoneyexample/tests/test_money.py
@@ -6,11 +6,9 @@ from src.themoneyexample.dollar import Dollar
 class TestMoney:
     def test_multiplication(self):
         five = Dollar(5)
-        product: Dollar = five.times(2)
-        assert 10 == product.amount
 
-        product: Dollar = five.times(3)
-        assert 15 == product.amount
+        assert Dollar(10) == five.times(2)
+        assert Dollar(15) == five.times(3)
 
     def test_equality(self):
         assert Dollar(5) == Dollar(5)
